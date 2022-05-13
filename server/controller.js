b684e95348c145680e78f05a5fd3c62fecae70aa
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-13 10:53:52 -0600

    getCountries created

--- server/controller.js
+++ server/controller.js
@@ -229,5 +229,10 @@ module.exports = {
             console.log('DB seeded!')
             res.sendStatus(200)
         }).catch(err => console.log('error seeding DB', err))
+    },
+    getCountries: (req, res) => {
+        sequelize.query(`
+        
+        `)
     }
 }
